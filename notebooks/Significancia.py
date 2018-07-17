27363bb296c914d5c6d493f53cdaf4ef772ac15a
sgrinschpun <sgrinschpun@ifae.es>
2018-07-17 17:28:27 +0200

    deleted data

--- notebooks/Significancia.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def CalcularSignificancia( On,Off ):
-    EventosON=np.sum(On[0:2])
-    EventosOFF=np.sum(Off[0:2])
-    Significancia=(EventosON-EventosOFF)/(EventosON+EventosOFF)**(0.5)
-    return Significancia
-
